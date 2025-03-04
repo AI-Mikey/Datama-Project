0f6b4eb90d32098bd4da9098703a716442997ca5
apcmmorenze <mmorenze@student.apc.edu.ph>
2025-03-04 13:53:20 +0800

    Delete asj-project directory

--- asj-project/v0-user-next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["localhost", "lcykqbuuwddnymseeryk.supabase.co"],
-  },
-}
-
-module.exports = nextConfig
-
